520090f8781f7d94ee3b2c554462a1fdcdd40a75
Niklas Högefjord <niklas@krokedil.se>
2018-09-04 12:22:59 +0200

    Added admin notice for https cehck and account settings check

--- classes/class-dibs-admin-notices.php
+++ classes/class-dibs-admin-notices.php
@@ -23,9 +23,12 @@ class DIBS_Easy_Admin_Notices {
 	public function check_settings() {
 		if ( ! empty( $_POST ) ) {
 			add_action( 'woocommerce_settings_saved', array( $this, 'check_terms' ) );
+			add_action( 'woocommerce_settings_saved', array( $this, 'check_account' ) );
 
 		} else {
 			add_action( 'admin_notices', array( $this, 'check_terms' ) );
+			add_action( 'admin_notices', array( $this, 'check_https' ) );
+			add_action( 'admin_notices', array( $this, 'check_account' ) );
 		}
 	}
 	/**
@@ -39,6 +42,41 @@ class DIBS_Easy_Admin_Notices {
 		if ( ! wc_get_page_id( 'terms' ) || wc_get_page_id( 'terms' ) < 0 ) {
 			echo '<div class="notice notice-error">';
 			echo '<p>' . sprintf(__( 'You need to <a href="%s" target="_blank">specify a terms page</a> in WooCommerce Settings to be able to use DIBS Easy.', 'dibs-easy-for-woocommerce' ), 'https://docs.woocommerce.com/document/configuring-woocommerce-settings/#section-14') . '</p>';
+			echo '</div>';
+		}
+	}
+
+	/**
+	 * Check if https is configured.
+	 */
+	public function check_https() {
+		if ( 'yes' !== $this->enabled ) {
+			return;
+		}
+		if ( ! is_ssl() ) {
+			echo '<div class="notice notice-error">';
+			echo '<p>' . esc_html( __( 'You need to enable and configure https to be able to use DIBS Easy.', 'dibs-easy-for-woocommerce' ) ) . '</p>';
+			echo '</div>';
+		}
+	}
+
+	/**
+	 * Check how account creation is set.
+	 */
+	public function check_account() {
+		if ( 'yes' !== $this->enabled ) {
+			return;
+		}
+		// Account page - username.
+		if ( 'yes' === get_option( 'woocommerce_enable_signup_and_login_from_checkout' ) && 'no' === get_option( 'woocommerce_registration_generate_username' ) ) {
+			echo '<div class="notice notice-error">';
+			echo '<p>' . sprintf( __( 'To be able to use DIBS Easy correctly you need to tick the checkbox <i>When creating an account, automatically generate a username from the customer\'s email address</i> when having the <i>Allow customers to create an account during checkout</i> setting activated. This can be changed in the <a href="%s">Accounts & Privacy tab</a>.', 'dibs-easy-for-woocommerce' ), admin_url( 'admin.php?page=wc-settings&tab=account' ) ) . '</p>';
+			echo '</div>';
+		}
+		// Account page - password.
+		if ( 'yes' === get_option( 'woocommerce_enable_signup_and_login_from_checkout' ) && 'no' === get_option( 'woocommerce_registration_generate_password' ) ) {
+			echo '<div class="notice notice-error">';
+			echo '<p>' . sprintf( __( 'To be able to use DIBS Easy correctly you need to tick the checkbox <i>When creating an account, automatically generate an account password</i> when having the <i>Allow customers to create an account during checkout</i> setting activated. This can be changed in the <a href="%s">Accounts & Privacy tab</a>.', 'dibs-easy-for-woocommerce' ), admin_url( 'admin.php?page=wc-settings&tab=account' ) ) . '</p>';
 			echo '</div>';
 		}
 	}
