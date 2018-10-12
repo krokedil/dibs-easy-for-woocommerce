673b9f56a02c4a092d468d0f13e5fa78d148c3c5
MichaelBengtsson <michael@krokedil.se>
2018-10-12 12:58:07 +0200

    Added webhooks again.

--- classes/requests/helpers/class-dibs-requests-get-notifications.php
+++ classes/requests/helpers/class-dibs-requests-get-notifications.php
@@ -11,13 +11,12 @@ class DIBS_Requests_Notifications {
 	}
 
 	public static function get_web_hooks() {
-		$web_hooks = array();
-		/*
+		$web_hooks   = array();
 		$web_hooks[] = array(
 			'eventName'     => 'payment.reservation.created',
 			'url'           => get_home_url() . '/wc-api/DIBS_WC_Payment_Created/',
 			'authorization' => wp_create_nonce( 'dibs_web_hooks' ),
-		);*/
+		);
 
 		return $web_hooks;
 	}
