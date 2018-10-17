fbbc8cc52ff6a319996496b162248a53ce1af61a
Niklas Högefjord <niklas@krokedil.se>
2018-10-17 15:08:35 +0200

    Logging improvements

--- classes/requests/class-dibs-requests-update-dibs-order-reference.php
+++ classes/requests/class-dibs-requests-update-dibs-order-reference.php
@@ -18,6 +18,7 @@ class DIBS_Requests_Update_DIBS_Order_Reference extends DIBS_Requests2 {
 		$request_url = $this->endpoint . 'payments/' . $this->payment_id . '/referenceinformation';
 
 		$response = wp_remote_request( $request_url, $this->get_request_args() );
+		DIBS_Easy::log( 'DIBS Update Order reference response (' . $request_url . '): ' . stripslashes_deep( json_encode( $response ) ) );
 		if ( is_wp_error( $response ) ) {
 			$this->get_error_message( $response );
 			return 'ERROR';
@@ -37,7 +38,7 @@ class DIBS_Requests_Update_DIBS_Order_Reference extends DIBS_Requests2 {
 			'method'  => 'PUT',
 			'body'    => json_encode( $this->request_body() ),
 		);
-		DIBS_Easy::log( 'DIBS Update Order request args: ' . stripslashes_deep( json_encode( $request_args ) ) );
+		DIBS_Easy::log( 'DIBS Update Order reference args: ' . stripslashes_deep( json_encode( $request_args ) ) );
 
 		return $request_args;
 	}
