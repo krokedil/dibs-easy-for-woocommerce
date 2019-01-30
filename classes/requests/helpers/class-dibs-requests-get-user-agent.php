323f33cb3252f50012c474b316928c33ec6d710c
Kristian-Andersson <krit_ian@hotmail.se>
2019-01-30 14:57:03 +0100

    URL convertion fix

--- classes/requests/helpers/class-dibs-requests-get-user-agent.php
+++ classes/requests/helpers/class-dibs-requests-get-user-agent.php
@@ -6,7 +6,7 @@ class DIBS_Requests_User_Agent extends DIBS_Requests2 {
 	public function get() {
 		$protocols  = array( 'http://', 'http://www.', 'https://', 'https://www.' );
 		$url        = str_replace( $protocols, '', get_bloginfo( 'url' ) );
-		$user_agent = apply_filters( 'dibs_easy_http_useragent', 'WordPress/' . get_bloginfo( 'version' ) . '; ' . idn_to_ascii( $url ) ) . ' - Plugin/' . WC_DIBS_EASY_VERSION . ' - PHP/' . phpversion() . ' - Krokedil';
+		$user_agent = apply_filters( 'dibs_easy_http_useragent', 'WordPress/' . get_bloginfo( 'version' ) . '; ' . iconv( 'UTF-8', 'ASCII//IGNORE', $url ) ) . ' - Plugin/' . WC_DIBS_EASY_VERSION . ' - PHP/' . phpversion() . ' - Krokedil';
 		return $user_agent;
 	}
 }
