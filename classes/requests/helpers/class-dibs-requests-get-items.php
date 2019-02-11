e9ddeab296a49ea4563c5294af8d34e51cdd9e9f
Kristian-Andersson <krit_ian@hotmail.se>
2019-02-11 12:00:45 +0100

    Improvement on product title for variable products

--- classes/requests/helpers/class-dibs-requests-get-items.php
+++ classes/requests/helpers/class-dibs-requests-get-items.php
@@ -32,18 +32,17 @@ class DIBS_Requests_Items {
 
 	public static function get_item( $cart_item ) {
 		if ( $cart_item['variation_id'] ) {
-			$product        = wc_get_product( $cart_item['variation_id'] );
-			$product_id     = $cart_item['variation_id'];
-			$cart_item_data = $cart_item['data'];
+			$product    = wc_get_product( $cart_item['variation_id'] );
+			$product_id = $cart_item['variation_id'];
 		} else {
-			$product        = wc_get_product( $cart_item['product_id'] );
-			$product_id     = $cart_item['product_id'];
-			$cart_item_data = $cart_item['data'];
+			$product    = wc_get_product( $cart_item['product_id'] );
+			$product_id = $cart_item['product_id'];
 		}
+		$item_name = wc_get_product( $product_id );
 
 		return array(
 			'reference'        => self::get_sku( $product, $product_id ),
-			'name'             => wc_dibs_clean_name( $cart_item_data->get_name() ),
+			'name'             => wc_dibs_clean_name( $item_name->get_name() ),
 			'quantity'         => $cart_item['quantity'],
 			'unit'             => __( 'pcs', 'dibs-easy-for-woocommerce' ),
 			'unitPrice'        => intval( round( $cart_item['line_total'] / $cart_item['quantity'], 2 ) * 100 ),
