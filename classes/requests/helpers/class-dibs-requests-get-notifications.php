067aacfb72d66f2533902d2abf563e8314b4faa1
Kristian-Andersson <krit_ian@hotmail.se>
2019-08-16 11:30:25 +0200

    Dont send web hooks to DIBS if host is local
    #60

--- classes/requests/helpers/class-dibs-requests-get-notifications.php
+++ classes/requests/helpers/class-dibs-requests-get-notifications.php
@@ -11,12 +11,17 @@ class DIBS_Requests_Notifications {
 	}
 
 	public static function get_web_hooks() {
-		$web_hooks       = array();
+		$web_hooks = array();
+
+		// Only set web hooks if host is not local.
+		if ( '127.0.0.1' !== $_SERVER['REMOTE_ADDR'] ) {
 			$web_hooks[] = array(
 				'eventName'     => 'payment.checkout.completed',
 				'url'           => add_query_arg( array( 'dibs-payment-created-callback' => '1' ), get_home_url() . '/wc-api/DIBS_Api_Callbacks/' ),
 				'authorization' => wp_create_nonce( 'dibs_web_hooks' ),
 			);
+		}
+
 		return $web_hooks;
 	}
 }
