6fed966e0b1852223798c2a3a95cbe9a1df1a445
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-03-19 11:09:12 +0100

    Only set web hooks if host is not local (127.0.0.1 or ::1)
    Creds to: Michael Svanström (squeezeday).

--- classes/requests/helpers/class-dibs-requests-get-notifications.php
+++ classes/requests/helpers/class-dibs-requests-get-notifications.php
@@ -13,8 +13,8 @@ class DIBS_Requests_Notifications {
 	public static function get_web_hooks() {
 		$web_hooks = array();
 
-		// Only set web hooks if host is not local.
-		if ( isset( $_SERVER['REMOTE_ADDR'] ) && '127.0.0.1' === $_SERVER['REMOTE_ADDR'] ) {
+		// Only set web hooks if host is not local (127.0.0.1 or ::1).
+		if ( isset( $_SERVER['REMOTE_ADDR'] ) && in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) {
 			return $web_hooks;
 		} else {
 			$web_hooks[] = array(
