2ef8345ccdf827644729662de91423a491b84aae
Miodrag Stojanovic <mio.sto@yandex.com>
2022-03-12 18:23:51 +0100

    update assets class

--- classes/class-dibs-assets.php
+++ classes/class-dibs-assets.php
@@ -27,10 +27,8 @@ class Dibs_Easy_Assets {
 	public function __construct() {
 		$this->settings = get_option( 'woocommerce_dibs_easy_settings', array() );
 		if ( ! ( empty( $this->settings ) ) && 'embedded' === $this->settings['checkout_flow'] ) {
-			// TODO move : woocommerce_checkout_update_order_meta action is just temporarily here.
-			add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'save_dibs_order_data' ), 10, 2 );
-			add_action( 'wp_enqueue_scripts', array( $this, 'dibs_load_js' ) );
-			add_action( 'wp_enqueue_scripts', array( $this, 'dibs_load_css' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'dibs_load_js' ), 10 );
+			add_action( 'wp_enqueue_scripts', array( $this, 'dibs_load_css' ), 10 );
 
 		}
 
@@ -39,7 +37,7 @@ class Dibs_Easy_Assets {
 	/**
 	 *
 	 * Checks whether a SCRIPT_DEBUG constant exists.
-	 * If there is, the plugin will use minified files.
+	 * If there is, the plugin will use non minified files.
 	 *
 	 * @return string
 	 */
@@ -69,6 +67,7 @@ class Dibs_Easy_Assets {
 		if ( 'yes' !== $settings['enabled'] ) {
 			return;
 		}
+		$test_mode = $settings['test_mode'];
 		if ( is_checkout() && ! is_wc_endpoint_url( 'order-pay' ) ) {
 			$script_url      = $this->get_script_url();
 			$dibs_payment_id = filter_input( INPUT_GET, 'dibs-payment-id', FILTER_SANITIZE_STRING );
@@ -82,30 +81,75 @@ class Dibs_Easy_Assets {
 				$checkout_initiated = 'yes';
 			} else {
 				$checkout_initiated = 'no';
+				$easy_confirm       = filter_input( INPUT_GET, 'easy_confirm', FILTER_SANITIZE_STRING );
+				if ( empty( $easy_confirm ) ) { //phpcs:ignore
+					dibs_easy_maybe_create_order();
+					$checkout_initiated = 'yes';
+				}
 			}
 
-			$standard_woo_checkout_fields = array( 'billing_first_name', 'billing_last_name', 'billing_address_1', 'billing_address_2', 'billing_postcode', 'billing_city', 'billing_phone', 'billing_email', 'billing_state', 'billing_country', 'billing_company', 'shipping_first_name', 'shipping_last_name', 'shipping_address_1', 'shipping_address_2', 'shipping_postcode', 'shipping_city', 'shipping_state', 'shipping_country', 'shipping_company', 'terms', 'account_username', 'account_password' );
+			$standard_woo_checkout_fields = array(
+				'billing_first_name',
+				'billing_last_name',
+				'billing_address_1',
+				'billing_address_2',
+				'billing_postcode',
+				'billing_city',
+				'billing_phone',
+				'billing_email',
+				'billing_state',
+				'billing_country',
+				'billing_company',
+				'shipping_first_name',
+				'shipping_last_name',
+				'shipping_address_1',
+				'shipping_address_2',
+				'shipping_postcode',
+				'shipping_city',
+				'shipping_state',
+				'shipping_country',
+				'shipping_company',
+				'terms',
+				'account_username',
+				'account_password',
+			);
 			$script_version               = $this->qoc_is_script_debug_enabled();
-			$src                          = WC_DIBS__URL . '/assets/js/checkout' . $script_version . '.js';
+			// $src                          = WC_DIBS__URL . '/assets/js/checkout' . $script_version . '.js';
 			wp_enqueue_script( 'dibs-script', $script_url, array( 'jquery' ), WC_DIBS_EASY_VERSION, false );
-			wp_register_script( 'checkout', $src, array( 'jquery', 'dibs-script' ), WC_DIBS_EASY_VERSION, false );
+			$src = WC_DIBS__URL . '/assets/js/dibs-easy-for-woocommerce' . $script_version . '.js';
+			wp_register_script(
+				'checkout',
+				$src,
+				array(
+					'jquery',
+					'dibs-script',
+				),
+				WC_DIBS_EASY_VERSION,
+				false
+			);
+			$private_key = $test_mode ? $settings['dibs_test_checkout_key'] : $settings['dibs_checkout_key'];
 			wp_localize_script(
 				'checkout',
-				'wc_dibs_easy',
+				'wcDibsEasy',
 				array(
-					'dibs_payment_id'                  => $dibs_payment_id,
+					'dibs_payment_id'                  => WC()->session->get( 'dibs_payment_id' ),
 					'paymentId'                        => $paymentId, // phpcs:ignore
 					'paymentFailed'                    => $paymentFailed, // phpcs:ignore
-					'checkout_initiated'               => $checkout_initiated,
+					'checkoutInitiated'                => $checkout_initiated,
 					'standard_woo_checkout_fields'     => $standard_woo_checkout_fields,
 					'dibs_process_order_text'          => __( 'Please wait while we process your order...', 'dibs-easy-for-woocommerce' ),
 					'required_fields_text'             => __( 'Please fill in all required checkout fields.', 'dibs-easy-for-woocommerce' ),
 					'update_checkout_url'              => WC_AJAX::get_endpoint( 'update_checkout' ),
-					'customer_adress_updated_url'      => WC_AJAX::get_endpoint( 'customer_adress_updated' ),
+					'customer_address_updated_url'     => WC_AJAX::get_endpoint( 'customer_address_updated' ),
 					'get_order_data_url'               => WC_AJAX::get_endpoint( 'get_order_data' ),
+					'submitOrder'                      => WC_AJAX::get_endpoint( 'checkout' ),
 					'dibs_add_customer_order_note_url' => WC_AJAX::get_endpoint( 'dibs_add_customer_order_note' ),
 					'change_payment_method_url'        => WC_AJAX::get_endpoint( 'change_payment_method' ),
+					'log_to_file_url'                  => WC_AJAX::get_endpoint( 'dibs_wc_log_js' ),
+					'log_to_file_nonce'                => wp_create_nonce( 'dibs_wc_log_js' ),
 					'nets_checkout_nonce'              => wp_create_nonce( 'nets_checkout' ),
+					'privateKey'                       => $private_key,
+					'locale'                           => wc_dibs_get_locale(),
 				)
 			);
 			wp_enqueue_script( 'checkout' );
@@ -126,13 +170,13 @@ class Dibs_Easy_Assets {
 		$style_version = $this->qoc_is_script_debug_enabled();
 		// Load stylesheet for the checkout page.
 		wp_register_style(
-			'qliro-one-style',
+			'dibs-style',
 			WC_DIBS__URL . '/assets/css/style' . $style_version . '.css',
 			array(),
 			WC_DIBS_EASY_VERSION
 		);
 
-		wp_enqueue_style( 'dibs_style' );
+		wp_enqueue_style( 'dibs-style' );
 	}
 }
 new Dibs_Easy_Assets();
