dbceef822870e0b78339dfb54712fbd955e02012
Montazar <github.v0its@slmail.me>
2025-03-14 11:07:03 +0100

    Rebuilds block assets

--- blocks/build/checkout.asset.php
+++ blocks/build/checkout.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-polyfill'), 'version' => '1376c8bab3f7501a629f');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-polyfill'), 'version' => 'af1ef786278b9b705dbe');
