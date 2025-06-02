191f62dcf35debc31dd368a18b0ecc95d90e1328
Louise-Krokedil <129946914+Louise-Krokedil@users.noreply.github.com>
2025-06-02 10:07:45 +0200

    Bumped WC version

--- dibs-easy-for-woocommerce.php
+++ dibs-easy-for-woocommerce.php
@@ -16,7 +16,7 @@
  * Text Domain:             dibs-easy-for-woocommerce
  * Domain Path:             /languages
  * WC requires at least:    5.6.0
- * WC tested up to:         9.7.0
+ * WC tested up to:         9.8.5
  * Copyright:               © 2017-2025 Krokedil AB.
  * License:                 GNU General Public License v3.0
  * License URI:             http://www.gnu.org/licenses/gpl-3.0.html
